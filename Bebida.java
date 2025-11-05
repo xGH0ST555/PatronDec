13e43e6769fc757b393025f74dc4748ad9adc33b
xGH0ST555 <dressdcucaracho@gmail.com>
2025-11-05 00:48:41 +0000

    Creacion del otro ejercicio y primer ejercicio funcionando

--- Bebida.java
+++ Bebida.java
@@ -14,4 +14,36 @@ abstract class ExtraDecorador implements Bebida {
     }
     public abstract double costo(); //Para sobreescribir el metodo en los extras
 }
-class 
+class Leche extends ExtraDecorador {
+    public Leche (Bebida bebida){
+        super(bebida);
+    }
+    @Override
+    public double costo(){
+        return bebida.costo() + 500; //Agrega el precio de la variacion (cafe con leche)        
+    }
+}
+class Crema extends ExtraDecorador{
+    public Crema (Bebida bebida){
+        super(bebida);
+    }
+    @Ovierride
+    public double costo(){
+        return bebida.costo() + 700; //Agrega el precio de la variacion (cafe con crema)
+    }
+}
+class Chocolate extends ExtraDecorador{
+    public Chocolate (Bebida bebida){
+        super(bebida);
+    }
+    @Overrride
+    public double costo(){
+        return bebida.costo() + 1000; //Agrega el precio de la variacion (cafe con chocolate)
+    }
+}
+public class BebidaMain {
+    public static void main(String[] args) {
+        Bebida miCafe = new Chocolate(new Leche (new Cafe())); //Cafe con leche y chocolate
+        System.out.println("El costo de su bebida es: " + miCafe.costo());
+    }
+}
